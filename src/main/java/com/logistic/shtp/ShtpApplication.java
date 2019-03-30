e3c5e86020ad0ce8a35370ac74e0e867be34ea80
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-03-30 12:45:26 +0700

    [ADD] init project

---
+++ src/main/java/com/logistic/shtp/ShtpApplication.java
@@ -0,0 +1,13 @@
+package com.logistic.shtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShtpApplication.class, args);
+	}
+
+}
